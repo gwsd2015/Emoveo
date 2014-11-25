57562cebb172b2ac66984fcce9f0fb2c0fb3c8b6
Voltekk <jwningli@gwu.edu>
2014-11-25 14:13:16 -0500

    Update 30Percent.pl

--- 30Percent.pl
+++ 30Percent.pl
@@ -5,6 +5,7 @@ use Text::Summarize::En;
 use Data::Dump qw(dump);
 use Algorithm::MarkovChain;
 use Path::Class;
+use Lingua::EN::Fathom;
 
 my $input = 'c:/Perl/finance.txt';
 open (INPUT, $input) or die;
@@ -16,47 +17,14 @@ my $output2 = 'c:/Perl/paragraphsummary.txt';
 open (OUTPUT2, '>'.$output2) or die;
 
 my $output3 = 'c:/Perl/markovout.txt';
-open (OUTPUT3, '>'\.$output3) or die;
-
-print "Enter the topic for generation: ";
-
-my $getuserinput = <STDIN>;
-
-chomp $getuserinput;
-
-my @inputs = qw(finance.txt paragraphsplit.txt paragraphsummary.txt); 
-    my $dir = dir(".");
-    my $f = "";
-    my @symbols = ();
-    foreach $f (@inputs) {
-        my $file = $dir->file($f);
-    	my $linecounter = 0;
-        my $wordcounter = 0;
-        my $file_handle = $file->openr();
-        while( my $line = $file_handle->getline() ) {
-    		chomp ($line);
-    		my @words = split(' ', $line);
-            push(@symbols, @words);
-    		$linecounter++;
-    		$wordcounter += scalar(@words);
-        }
-    	print "$linecounter lines, $wordcounter words read from $f\n";
-    }   
-    my $chain = Algorithm::MarkovChain::->new();
-    $chain->seed(symbols => \@symbols, longest => 6);
-    foreach (1 .. 20) {
-        my @newness = $chain->spew(length   => 100,
-                                   complete => [ $getuserinput ]);
-        print OUTPUT3 join (" ", @newness), ".\n\n";
-    }
+open (OUTPUT3, '>'.$output3) or die;
 
 while(<INPUT>){
 	tr/A-Z/a-z/;
 	tr/.,:;!&?"'(){}\-\$\+\=\{\}\@\/\*\>\<//d;
 	s/[0-9]//g;
 	my @paragraph = split(/\n/, $_);
-	print OUTPUT "$_" for @paragraph;
-	
+	print OUTPUT "$_" for @paragraph;	
 	
 =pod
 	foreach my $para (@paragraph){
@@ -100,7 +68,56 @@ while ( length($buffer) < $size ) {
 	
 }
 
+print "Enter the topic for generation: ";
+
+my $getuserinput = <STDIN>;
+
+chomp $getuserinput;
+
+my @inputs = qw(finance.txt forensics.txt); 
+    my $dir = dir(".");
+    my $f = "";
+    my @symbols = ();
+    foreach $f (@inputs) {
+        my $file = $dir->file($f);
+    	my $linecounter = 0;
+        my $wordcounter = 0;
+        my $file_handle = $file->openr();
+        while( my $line = $file_handle->getline() ) {
+    		chomp ($line);
+    		my @words = split(' ', $line);
+            push(@symbols, @words);
+    		$linecounter++;
+    		$wordcounter += scalar(@words);
+        }
+    	print "$linecounter lines, $wordcounter words read from $f\n";
+    }   
+    my $chain = Algorithm::MarkovChain::->new();
+    $chain->seed(symbols => \@symbols, longest => 6);
+    foreach (1 .. 20) {
+        my @newness = $chain->spew(length   => 100,
+                                   complete => [ $getuserinput ]);
+	print @newness;
+	print "\n\nWould you like to keep this? \t\t";
+	my $yesno = <STDIN>;
+	chomp $yesno;
+	if($yesno =~ /[Yy][Ee][Ss]\n?/){
+        	print OUTPUT3 join (" ", @newness), ".\n\n";
+		while (<OUTPUT3>){
+			s/\]/ /g;
+			s/\[/ /g;
+		}
+	}
+    }
 
 close INPUT;
 close OUTPUT;
 close OUTPUT2;
+close OUTPUT3;
+
+my $text_for_analysis = new Lingua::EN::Fathom;
+$text_for_analysis -> analyse_file('c:/perl/markovout.txt');
+print "Preparing statistics on generated paragraphs: \n\n\n";
+print $text_for_analysis -> report;
+
+
