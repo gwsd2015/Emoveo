88dd07072cdf0b9f63cd31766888f60f19de86b1
Voltekk <jwningli@gwu.edu>
2015-02-23 11:11:32 -0500

    Update 2232015.pl

--- 2232015.pl
+++ 2232015.pl
@@ -185,16 +185,63 @@ print "File storing the unique words is now available at $wordlist \n";
 
 close WORD;
 
-my $ngramfile = "C:/Perl/ngram.txt";
-open(NGRAM, '>.'.$ngramfile) or die "Can't create file to store ngrams.\n";
+my $twogramfile = "C:/Perl/2gram.txt";
+open(TWOGRAM, '>'.$twogramfile) or die "Can't create file to store 2grams.\n";
 #bigrams to sevengrams to sort
 my $ngram = Lingua::EN::Ngram->new(file => $check);
 my $bigrams = $ngram->ngram(2);
-foreach my $bigram(sort {$$bigrams{my $b} <=> $$bigrams{my $a} keys my %$bigrams){
-	print NGRAM $$bigrams{$bigram}, "\t$bigram\n";
+foreach my $bigram(sort {$$bigrams{my $b} <=> $$bigrams{my $a}} keys %$bigrams){
+	print TWOGRAM $$bigrams{$bigram}, "\t$bigram\n";
 }
+print "Bigrams created...\n";
+close TWOGRAM;
 system('pause');
-close NGRAM;
+my $threegramfile = "C:/Perl/3gram.txt";
+open(THREEGRAM, '>'.$threegramfile) or die "Can't create file to store 3grams.\n";
+my $trigrams = $ngram->ngram(3);
+foreach my $trigram(sort {$$trigrams{my $b} <=> $$trigrams{my $a}} keys %$trigrams){
+	print THREEGRAM $$trigrams{$trigram}, "\t$trigram\n";
+}
+print "Trigrams created...\n";
+close THREEGRAM;
+system('pause');
+my $fourgramfile = "C:/Perl/4gram.txt";
+open(FOURGRAM, '>'.$fourgramfile) or die "Can't create file to store 4grams.\n";
+my $fourgrams = $ngram->ngram(4);
+foreach my $fourgram(sort {$$fourgrams{my $b} <=> $$fourgrams{my $a}} keys %$fourgrams){
+	print FOURGRAM $$fourgrams{$fourgram}, "\t$fourgram\n";
+}
+print "Fourgrams created...\n";
+close FOURGRAM;
+system('pause');
+my $fivegramfile = "C:/Perl/5gram.txt";
+open(FIVEGRAM, '>'.$fivegramfile) or die "Can't create file to store 5grams.\n";
+my $fivegrams = $ngram->ngram(5);
+foreach my $fivegram(sort {$$fivegrams{my $b} <=> $$fivegrams{my $a}} keys %$fivegrams){
+	print FIVEGRAM $$fivegrams{$fivegram}, "\t$fivegram\n";
+}
+print "Fivegrams created...\n";
+close FIVEGRAM;
+system('pause');
+my $sixgramfile = "C:/Perl/6gram.txt";
+open(SIXGRAM, '>'.$sixgramfile) or die "Can't create file to store 6grams.\n";
+my $sixgrams = $ngram->ngram(6);
+foreach my $sixgram(sort {$$sixgrams{my $b} <=> $$sixgrams{my $a}} keys %$sixgrams){
+	print SIXGRAM $$sixgrams{$sixgram}, "\t$sixgram\n";
+}
+print "Sixgrams created...\n";
+close SIXGRAM;
+system('pause');
+my $sevengramfile = "C:/Perl/7gram.txt";
+open(SEVENGRAM, '>'.$sevengramfile) or die "Can't create file to store 7grams.\n";
+my $sevengrams = $ngram->ngram(7);
+foreach my $sevengram(sort {$$sevengrams{my $b} <=> $$sevengrams{my $a}} keys %$sevengrams){
+	print SEVENGRAM $$sevengrams{$sevengram}, "\t$sevengram\n";
+}
+print "Sevengrams created...\n";
+close SEVENGRAM;
+system('pause');
+
 
 my $splitstorage = "C:/Perl/splitfile.txt";
 open(SSTORE, '>'.$splitstorage) or die "Can't create file to store the subsections.\n";
