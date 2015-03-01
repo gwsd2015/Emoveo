389fd327beea17858b45485c2dd1df6a78486015
Voltekk <jwningli@gwu.edu>
2015-03-01 11:36:43 -0500

    Update 3012015.pl

--- 3012015.pl
+++ 3012015.pl
@@ -728,11 +728,11 @@ dump %languages = langof($datatomod);
 print "\nThe probability of this text being English is:\t$languages{'en'}\n\n...\n";
 if($languages{'en'} > 0.357){
 	print "\nLanguage analysis completed.\n";	
-	print "\nSecond stage test have determined redaction is incomplete.\n\";
+	print "\nSecond stage test have determined redaction is incomplete.\n";
 	system('pause');
 } else {
 	print "\nLanguage analysis completed.\n";
-	print "nPassed second stage analysis.\n";
+	print "\nPassed second stage analysis.\n";
 }
 
 #use this instead if CLD ever installs properly
