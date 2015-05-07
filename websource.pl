2435933687514a8a94122fc926accbaf204def05
Voltekk <jwningli@gwu.edu>
2015-05-07 16:14:02 -0400

    Delete websource.pl

--- websource.pl
+++
@@ -1,9 +0,0 @@
-﻿use strict;
-use warnings;
-use LWP::Simple;
-
-my $URL = get("http://edition.cnn.com/2014/09/30/world/asia/china-hong-kong-protests/index.html?hpt=ias_c1/");
-
-open(FILE, ">c:/perl64/file.txt");
-print FILE $URL;
-close(FILE);
